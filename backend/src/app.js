350a3a12a93dda09f4d363ae10fff684e42a5fe0
Nikunj Malwana <makwananik7005@gmail.com>
2026-01-26 00:19:29 +0530

    fix: update CORS origin to use HTTPS for vidplay-frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'vidplay-frontend.vercel.app'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://vidplay-frontend.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
